0cd1184656b87e6b115cd0a76caf033bfb5ca204
Unknown <kwangkg.30@gmail.com>
2017-11-21 17:25:47 +0700

    ทำข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,5 +4,9 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
+
+  cout << "Miss. " << "Phonphimon Sabaibang" << endl << "Ninkname kwang"  << endl << "Sex female " << endl
+  << "Born on January 30, 1999." << endl << "Hobby Music"  << endl << "Status single" ;
+
   return 0;
 }
